fbaebcafdbd8282da78f3ae37c1ed1850969c40a
dvitel <viteldmitry@gmail.com>
2025-11-02 20:16:18 -0500

    change of prompt back to one mutation

--- t_search/operators/mutation/llmm.py
+++ t_search/operators/mutation/llmm.py
@@ -41,7 +41,6 @@ class PromptContext:
     fitness: float
     op_desc: dict[str, str]        
     free_vars: Sequence[str]
-    num_mutations: int = 3
     num_positions: int = 2
     loss_name: str
 
@@ -117,8 +116,7 @@ Hardest tests:
     {% endfor %}
 {{loss_name}} is {{ fitness | round(5) }}.
 
-Generate {{num_mutations}} alternative mutations for the current term. 
-For each mutation:
+Generate the mutated term by following steps:
 1. First, select from one to {{num_positions}} subexpressions to modify and provide a short reason for selection.
 2. Then, decide the change that improves tests and loss (operator replacement, constant adjustment, subtree replacement, etc.).
 3. Finnaly, apply the mutation and provide the resulting LISP expression.
@@ -136,10 +134,6 @@ class MutationExecution:
     all_selected_positions: Annotated[list[MutationPositionExecution], "List of positions selected for mutation."]
     final_mutated_term: Annotated[str, "Final LISP expression after applying the mutations to the selected positions."]
 
-@dataclass
-class AllMutationExecutions:
-    mutations: Annotated[list[MutationExecution], "List of all mutation executed independently."]
-
 class LLMM(TermMutation):
 
     def __init__(self, name = "LLMM", *, 
@@ -148,7 +142,6 @@ class LLMM(TermMutation):
                 #  prompt_template_path: str = "",
                  prompt_template: str = tests_prompt,
                  op_desc: dict[str, str] = llm.ops_descriptions,
-                 num_mutations: int = 3,
                  max_num_positions: int = 2,
                  max_num_examples: int = 3,
                  max_num_tests: int = 3,
@@ -156,7 +149,6 @@ class LLMM(TermMutation):
                  loss_name: str = "NMSE",
                  **kwargs):
         super().__init__(name, rate=rate, **kwargs)
-        self.num_mutations = num_mutations
         self.max_num_positions = max_num_positions
         self.max_num_examples = max_num_examples
         self.max_num_tests = max_num_tests
@@ -209,7 +201,6 @@ class LLMM(TermMutation):
 
         demonstrations = self.select_exemplars(solver, term)
         context = PromptContext(
-            num_mutations=self.num_mutations,
             num_positions=self.max_num_positions,
             term = str(term),
             tests = tests,
@@ -228,46 +219,36 @@ class LLMM(TermMutation):
             return None
 
         try:
-            response = self.llm(prompt, AllMutationExecutions)
+            response = self.llm(prompt, MutationExecution)
         except Exception as e:
             print("Error during LLM prompting:", e)
             self.add_metric(llm_error=1)
             return None
 
-        candidates = []
-        for mutation in response.mutations:
-            new_term = solver.parse_term_str(mutation.final_mutated_term)
-            if new_term is None:
-                self.add_metric(llm_syn_invalid=1)
-                continue
-            if not solver.is_valid(new_term):
-                self.add_metric(llm_constr_invalid=1)
-                continue
-            candidates.append(new_term)
-
-        if len(candidates) == 0:
+        new_term = solver.parse_term_str(response.final_mutated_term)
+        if new_term is None:
+            self.add_metric(llm_syn_invalid=1)
             return None
+        if not solver.is_valid(new_term):
+            self.add_metric(llm_constr_invalid=1)
+            return None 
         
-        outputs, fitnesses = solver.eval(candidates, return_outputs="list", return_fitness="list")
+        outputs, fitnesses = solver.eval(new_term, return_outputs="list", return_fitness="list")
 
-        best_id = torch.argmin(fitnesses).item()
-        best_term = candidates[best_id]
-        best_fitness = fitnesses[best_id].item()
-        best_outcomes = outputs[best_id]
+        new_fitness = fitnesses[0]
+        new_outcomes = outputs[0]
+        good_mutations=1 if new_fitness < fitness else 0, 
+        bad_mutations=1 if new_fitness > fitness else 0,
+        neutral_mutations=1 if new_fitness == fitness else 0
 
-        good_mutation_count = len(f for f in fitnesses if f < fitness)
-        bad_mutation_count = len(f for f in fitnesses if f > fitness)
-        neutral_mutation_count = len(f for f in fitnesses if f == fitness)
         self.add_metric(
-            good_mutations=good_mutation_count, 
-            bad_mutations=bad_mutation_count, 
-            neutral_mutations=neutral_mutation_count
+            good_mutations=good_mutations,
+            bad_mutations=bad_mutations,
+            neutral_mutations=neutral_mutations
         )
 
-        if best_fitness < fitness: # good mutation - record it 
-
-            # selecting tests with best outcome improvements 
-            outcome_diffs_after = torch.abs(target - best_outcomes)
+        if good_mutations == 1:
+            outcome_diffs_after = torch.abs(target - new_outcomes)
             outcome_improvements = outcome_diffs - outcome_diffs_after
             test_ids = torch.argsort(outcome_improvements)
             best_test_ids = test_ids[-self.max_num_demo_tests:]
@@ -275,23 +256,23 @@ class LLMM(TermMutation):
             final_best_test_ids = best_test_ids[best_test_id_ids]
             best_expected = target[final_best_test_ids].tolist()
             best_actual = outcomes[final_best_test_ids].tolist()
-            best_actual_after = best_outcomes[final_best_test_ids].tolist()
+            best_actual_after = new_outcomes[final_best_test_ids].tolist()
             best_free_vars = {var_name:var_values[final_best_test_ids].tolist() for var_name, var_values in solver.var_binding}
 
             tests = [TestCaseContext(vars={var_name: best_free_vars[var_name][i] for var_name in solver.vars.keys()},
-                                     expected=best_expected[i],
-                                     actual=best_actual[i],
-                                     diff=best_actual[i] - best_expected[i],
-                                     actual_after=best_actual_after[i],
-                                     diff_after=best_actual_after[i] - best_expected[i]
-                                     )
-                     for i in final_best_test_ids]
+                                        expected=best_expected[i],
+                                        actual=best_actual[i],
+                                        diff=best_actual[i] - best_expected[i],
+                                        actual_after=best_actual_after[i],
+                                        diff_after=best_actual_after[i] - best_expected[i]
+                                        )
+                        for i in final_best_test_ids]
             good_mutation = GoodMutationContext(
                 term = context.term, 
-                term_after = str(best_term),
-                fitness_diff = fitness - best_fitness,
+                term_after = str(new_term),
+                fitness_diff = fitness - new_fitness,
                 tests = tests
             )
             self.good_mutations.append(good_mutation)
         
-        return best_term 
+        return new_term 
