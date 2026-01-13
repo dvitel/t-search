a5bdffe2323fca1964a16c27d001255ce702ac80
dvitel <viteldmitry@gmail.com>
2026-01-13 13:49:22 -0500

    koza2 colab

---
+++ colab/koza2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+config='koza_set2'
+
+datasets=('r_1' 'r_2' 'keijzer_3' 'keijzer_4' 'keijzer_11' 'nguyen_12' 'pagie_1' 'vladislavleva_1' 'koza_3' 'keijzer_6' 'vladislavleva_8' 'korns_13' 'korns_14' 'korns_15')
+
+for seed in {0..9}; do
+    for dataset in "${datasets[@]}"; do
+        echo "====================================================================="
+        echo "Running t-search with dataset=$dataset, config=$config, seed=$seed"
+
+        python -m t_search \
+            --dataset $dataset \
+            --config ./configs/$config.json \
+            --output ./data/results.jsonlist \
+            --device cuda \
+            --dtype float32 \
+            --seed $seed
+
+        echo "Finished t-search with dataset=$dataset, config=$config, seed=$seed"
+    done 
+done
