cb430f29222f40d3c81542bd3cd74f5eca7b5fff
dvitel <viteldmitry@gmail.com>
2026-02-22 19:49:23 -0500

    fix of nwf

--- slurm/point_optim_fix.sh
+++ slurm/point_optim_fix.sh
@@ -6,29 +6,30 @@
 #SBATCH --mem=32G
 #SBATCH --gpus=1 # 1 GPU
 #SBATCH -p snsm_itn19
-#SBATCH --array=0-3
+#SBATCH --array=0-7
 #SBATCH --exclude=mdc-1057-28-15,mdc-1057-27-18
 
 ## SBATCH --open-mode=append
 
 module purge
 
-config='point_optim'
+config='point_optim_nwf'
 
-seeds=(11 13 16 22)
-datasets=('korns_14' 'korns_13' 'korns_15' 'korns_14') 
+seeds=(16 23 24 25 26 27 28 29)
+datasets=('r_1' 'r_2' 'keijzer_3' 'keijzer_4' 'keijzer_11' 'nguyen_12' 'pagie_1' 'vladislavleva_1' 'koza_3' 'keijzer_6' 'vladislavleva_8' 'korns_13' 'korns_14' 'korns_15')
 seed=${seeds[$SLURM_ARRAY_TASK_ID]}
-dataset=${datasets[$SLURM_ARRAY_TASK_ID]}
 
-echo "====================================================================="
-echo "Running t-search with dataset=$dataset, config=$config, seed=$seed"
+for dataset in "${datasets[@]}"; do
+    echo "====================================================================="
+    echo "Running t-search with dataset=$dataset, config=$config, seed=$seed"
 
-$HOME/t-search/t-search-env/bin/python -m t_search \
-    --dataset $dataset \
-    --config $HOME/t-search/configs/$config.json \
-    --output $WORK_BGFS/t-search/results.jsonlist \
-    --device cuda \
-    --dtype float32 \
-    --seed $seed
+    $HOME/t-search/t-search-env/bin/python -m t_search \
+        --dataset $dataset \
+        --config $HOME/t-search/configs/$config.json \
+        --output $WORK_BGFS/t-search/results.jsonlist \
+        --device cuda \
+        --dtype float32 \
+        --seed $seed
 
-echo "Finished t-search with dataset=$dataset, config=$config, seed=$seed"
+    echo "Finished t-search with dataset=$dataset, config=$config, seed=$seed"
+done
