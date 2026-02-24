7cff092f6991d63e6c3a30ac3daa997260841903
dvitel <viteldmitry@gmail.com>
2026-02-23 23:34:43 -0500

    po2r

---
+++ slurm/point_optim_2r.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+#SBATCH --job-name=po2r
+#SBATCH --time=72:00:00
+#SBATCH --output=po2r-%a.out
+#SBATCH --error=po2r-%a.out
+#SBATCH --mem=32G
+#SBATCH --gpus=1 # 1 GPU
+#SBATCH -p snsm_itn19
+#SBATCH --array=0-29
+#SBATCH --exclude=mdc-1057-28-15,mdc-1057-27-18
+
+## SBATCH --open-mode=append
+
+module purge
+
+config='point_optim_2r'
+
+seed=$SLURM_ARRAY_TASK_ID
+datasets=('r_1' 'r_2' 'keijzer_3' 'keijzer_4' 'keijzer_11' 'nguyen_12' 'pagie_1' 'vladislavleva_1' 'koza_3' 'keijzer_6' 'vladislavleva_8' 'korns_13' 'korns_14' 'korns_15')
+
+for dataset in "${datasets[@]}"; do
+    echo "====================================================================="
+    echo "Running t-search with dataset=$dataset, config=$config, seed=$seed"
+
+    $HOME/t-search/t-search-env/bin/python -m t_search \
+        --dataset $dataset \
+        --config $HOME/t-search/configs/$config.json \
+        --output $WORK_BGFS/t-search/results.jsonlist \
+        --device cuda \
+        --dtype float32 \
+        --seed $seed
+
+    echo "Finished t-search with dataset=$dataset, config=$config, seed=$seed"
+done 
