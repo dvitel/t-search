3baf5aef96ce5821c0d2af238bae25a2dea0d4d0
dvitel <viteldmitry@gmail.com>
2026-02-24 22:25:42 -0500

    fix

--- slurm/point_optim_fix.sh
+++ slurm/point_optim_fix.sh
@@ -13,10 +13,10 @@
 
 module purge
 
-config='point_optim_2'
+config='point_optim_2r'
 
-seeds=(7 18 12)
-datasets=('korns_13' 'korns_13' 'korns_15')
+seeds=(11 11 6 9)
+datasets=('korns_14' 'korns_15' 'korns_13' 'korns_15')
 seed=${seeds[$SLURM_ARRAY_TASK_ID]}
 dataset=${datasets[$SLURM_ARRAY_TASK_ID]}
 
