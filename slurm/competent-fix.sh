dc033b7ef0357729c167750ee17b28823ee8aaf4
dvitel <viteldmitry@gmail.com>
2026-02-28 21:22:28 -0500

    competent seed 22

--- slurm/competent-fix.sh
+++ slurm/competent-fix.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
-#SBATCH --job-name=co
+#SBATCH --job-name=cof
 #SBATCH --time=72:00:00
-#SBATCH --output=co-%a.out
-#SBATCH --error=co-%a.out
+#SBATCH --output=cof-%a.out
+#SBATCH --error=cof-%a.out
 #SBATCH --mem=64G
 #SBATCH --gpus=1 # 1 GPU
 #SBATCH -p snsm_itn19
-#SBATCH --array=0-1
+#SBATCH --array=0-7
 #SBATCH --exclude=mdc-1057-28-15,mdc-1057-27-18
 
 ## SBATCH --open-mode=append
@@ -15,21 +15,20 @@ module purge
 
 config='competent'
 
-seeds=(19 20)
+seeds=(22 22 22 22 22 22 22 22)
 seed=${seeds[$SLURM_ARRAY_TASK_ID]}
-datasets=('r_1' 'r_2' 'keijzer_3' 'keijzer_4' 'keijzer_11' 'nguyen_12' 'pagie_1' 'vladislavleva_1' 'koza_3' 'keijzer_6' 'vladislavleva_8' 'korns_13' 'korns_14' 'korns_15')
+datasets=('pagie_1' 'vladislavleva_1' 'koza_3' 'keijzer_6' 'vladislavleva_8' 'korns_13' 'korns_14' 'korns_15')
+dataset=${datasets[$SLURM_ARRAY_TASK_ID]}
 
-for dataset in "${datasets[@]}"; do
-    echo "====================================================================="
-    echo "Running t-search with dataset=$dataset, config=$config, seed=$seed"
+echo "====================================================================="
+echo "Running t-search with dataset=$dataset, config=$config, seed=$seed"
 
-    $HOME/t-search/t-search-env/bin/python -m t_search \
-        --dataset $dataset \
-        --config $HOME/t-search/configs/$config.json \
-        --output $WORK_BGFS/t-search/results.jsonlist \
-        --device cuda \
-        --dtype float32 \
-        --seed $seed
+$HOME/t-search/t-search-env/bin/python -m t_search \
+    --dataset $dataset \
+    --config $HOME/t-search/configs/$config.json \
+    --output $WORK_BGFS/t-search/results.jsonlist \
+    --device cuda \
+    --dtype float32 \
+    --seed $seed
 
-    echo "Finished t-search with dataset=$dataset, config=$config, seed=$seed"
-done 
+echo "Finished t-search with dataset=$dataset, config=$config, seed=$seed"
